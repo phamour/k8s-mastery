0abd0ef748aad453c9833f4386c00da7f308a6b3
yipengh <huang.ypeng@gmail.com>
2020-12-12 16:10:58 +0800

    Change frontend url pointing webapp DNS

--- sa-frontend/src/App.js
+++ sa-frontend/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     };
 
     analyzeSentence() {
-        fetch('http://localhost:8080/sentiment', {
+        fetch('http://192.168.31.30:30008/sentiment', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
